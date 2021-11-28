bb336e10010076b6f610e3923be8484fc5656d02
Casper Kristiansson <casper.kristiansson@yahoo.se>
2021-11-28 15:20:01 +0100

    Add getTopRatedMovies

--- public/js/apiFetch.js
+++ public/js/apiFetch.js
@@ -1,13 +1,17 @@
 const ApiFetch = {
     apiCall(params) {
-        return fetch(BASE_URL + params + "?api_key=" + API_KEY).then(
+        return fetch(BASE_URL + params + "&api_key=" + API_KEY).then(
             (response) =>
                 response.ok ? response.json() : throwError(response.statusText)
         );
     },
 
     getTopMovies(page = 1) {
-        return this.apiCall(`/movie/popular`);
+        return this.apiCall(`/movie/popular?${page}`);
+    },
+
+    getTopRatedMovies(page = 1) {
+        return this.apiCall(`/movie/top_rated?${page}`);
     }
 };
 
