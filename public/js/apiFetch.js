6ebc07842c5e5c411e640e86ee9e40e8d8d53c84
MichellDib <michell.dib28@gmail.com>
2021-11-28 16:13:23 +0100

    Movie-related apiCalls
    Multiple apiCalls for different categories made

--- public/js/apiFetch.js
+++ public/js/apiFetch.js
@@ -1,17 +1,47 @@
 const ApiFetch = {
     apiCall(params) {
-        return fetch(BASE_URL + params + "&api_key=" + API_KEY).then(
+        return fetch(BASE_URL + params + "api_key=" + API_KEY).then(
             (response) =>
                 response.ok ? response.json() : throwError(response.statusText)
         );
     },
 
     getTopMovies(page = 1) {
-        return this.apiCall(`/movie/popular?${page}`);
+        return this.apiCall(`/movie/popular?${page}&`);
     },
 
     getTopRatedMovies(page = 1) {
-        return this.apiCall(`/movie/top_rated?${page}`);
+        return this.apiCall(`/movie/top_rated?page=${page}&`);
+    },
+    getMovieDetails(id = 566525) {
+        return this.apiCall(`/movie/${id}?`);
+    },
+    getMovieKeywords(id = 566525) {
+        return this.apiCall(`/movie/${id}/keywords?`);
+    },
+    getPopularMovies(page = 1) {
+        return this.apiCall(`/movie/popular?`);
+    },
+    getMovieRecommendations(id = 566525){
+        return this.apiCall(`/movie/${id}/recommendations?`);
+    },
+    getMovieCredits(id = 566525){
+        return this.apiCall(`/movie/${id}/credits?`);
+    },
+    getMovieReviews(id = 566525){
+        return this.apiCall(`/movie/${id}/reviews?`);
+    },
+    getMovieReleaseDates(id = 566525){
+        return this.apiCall(`/movie/${id}/release_dates?`);
+    },
+    getLatestMovie(){
+        return this.apiCall(`/movie/latest?`);
+    },
+    getNowPlayingMovies(page = 1){
+        return this.apiCall(`/movie/now_playing?`);
+    },
+    getUpcomingMovies(page = 1){
+        return this.apiCall(`/movie/upcoming?`);
     }
 };
 
