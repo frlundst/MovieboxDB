cbf970d8f3b8ed521142b30928be24b8678241ba
Casper Kristiansson <casper.kristiansson@yahoo.se>
2021-12-01 11:56:33 +0100

    Top movies score add, change background

--- src/js/views/homeMoviesView.js
+++ src/js/views/homeMoviesView.js
@@ -6,22 +6,25 @@ function HomeMoviesView(props) {
         console.log(props.movies),
         (
             <div className="top-movies-section">
-                {props.movies.slice(0, 12).map(function (movie) {
-                    return (
-                        <div className="movie-card" key={movie.id}>
-                            <img
-                                src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
-                                alt={movie.title}
-                            />
-                            <div className="movie-vote">
-                                <p>{movie.vote_average ? `⭐${movie.vote_average}` : "⠀"}</p>
+                <h1>Top Movies</h1>
+                <div className="top-movies-container">
+                    {props.movies.slice(0, 6).map(function (movie) {
+                        return (
+                            <div className="movie-card" key={movie.id}>
+                                <div className="movie-vote">
+                                    <p>{movie.vote_average ? `${movie.vote_average}` : "?"}</p>
+                                </div>
+                                <img
+                                    src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
+                                    alt={movie.title}
+                                />
+                                <div className="movie-card-info">
+                                    <h3>{movie.title}</h3>
+                                </div>
                             </div>
-                            <div className="movie-card-info">
-                                <h3>{movie.title}</h3>
-                            </div>
-                        </div>
-                    );
-                })}
+                        );
+                    })}
+                </div>
             </div>
         )
     );
