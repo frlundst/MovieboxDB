b90290ec062ffff1c2b0c4d367c2879288dad255
Casper Kristiansson <casper.kristiansson@yahoo.se>
2021-12-01 07:37:28 +0100

    Add resizing for homeView intro image

--- src/js/views/homeView.js
+++ src/js/views/homeView.js
@@ -4,7 +4,7 @@ import '../../css/homeView.css';
 function HomeView(props) {
     return (
         <div className="intro-section">
-            <img src="/images/batmanHomePage.jpg" alt="Melanie Jansen Pic" />
+            <img src="/images/batmanHomePage.jpg" className="center-fit" alt="Melanie Jansen Pic" />
             <div className="intro-text">
                 <h1>MovieBoxDB</h1>
                 <p>Movie Tracking Application</p>
