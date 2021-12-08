325913882bba71eb9abd36b0938565a80bc34150
MichellDib <michell.dib28@gmail.com>
2021-12-08 16:37:51 +0100

    LoginPresenter Auth

--- src/js/presenter/loginPresenter.js
+++ src/js/presenter/loginPresenter.js
@@ -1,13 +1,26 @@
 import React from "react";
 import LoginView from "../views/loginView";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
-function LoginPresenter(props){
+function LoginPresenter(props) {
     /*const signupForm = document.querySelector('#login-section');
     signupForm.addEventListener('submit',(e)=>{
         e.preventDefault();
         const email = signupForm['email-input'];
     })*/
-    return(
+    var auth = getAuth();
+    createUserWithEmailAndPassword(auth, 'michell.dib@gmail.com', 'abc123')
+        .then((userCredential) => {
+            // Signed in 
+            const user = userCredential.user;
+            // ...
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            // ..
+        });
+    return (
         <div>
             <LoginView></LoginView>
         </div>
